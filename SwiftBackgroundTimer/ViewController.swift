a3c00c2a0cde562ec266a7042970c90a725cdc5f
Arik Segal <aharon.s1@meuhedet.co.il>
2023-01-10 14:18:39 +0200

    Added Project

---
+++ SwiftBackgroundTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBackgroundTimer
+//
+//  Created by Arik Segal on 10/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
