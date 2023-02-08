a5064f43a54e2a600ace61ac51d42d1c70076edc
Arik Segal <aharon.s1@meuhedet.co.il>
2023-02-08 16:27:47 +0200

    Hello World

---
+++ SwiftBackgroundTimer/SwiftBackgroundTimer/SwiftBackgroundTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBackgroundTimerApp.swift
+//  SwiftBackgroundTimer
+//
+//  Created by Arik Segal on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBackgroundTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
